1f4488d5f3e5d84444ce545d474aa5faa3e631ff
Zachary Schroeder <zschroeder34@gmail.com>
2023-02-06 13:44:30 -0600

    fixed an error

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-async function getData() {
+async function getTestData() {
   const response = await fetch("https://dev.foleyprep.com/interview/");
   if (response.ok) {
     const data = await response.json();
     return data;
   } else {
-    throw new Error("Failed to fetch data");
+    throw new Error("Failed");
   }
 }
 
-function gradeTest(data) {
-  const result = [];
-  for (const user of data.users) {
-    const test = data.tests.find((t) => t.name === user.test);
-    if (!test) {
-      throw new Error(`Test not found: ${user.test}`);
+function gradedTests(data) {
+    const result = [];
+    for (let i = 0; i < data.users.length; i++) {
+      const user = data.users[i];
+      let test = null;
+      for (let j = 0; j < data.tests.length; j++) {
+        if (data.tests[j].name === user.test) {
+          test = data.tests[j];
+          break;
+        }
+      }
+      if (!test) {
+        throw new Error(`Test not found: ${user.test}`);
+      }
+      let score = 0;
+      for (let k = 0; k < user.responses.length; k++) {
+        if (user.responses[k] === test.answers[k]) {
+          score++;
+        }
+      }
+      result.push({
+        name: user.name,
+        test: user.test,
+        score: (score / test.answers.length) * 100,
+      });
     }
-    const score = user.responses.reduce((acc, response, index) => {
-      return response === test.answers[index] ? acc + 1 : acc;
-    }, 0);
-    result.push({
-      name: user.name,
-      test: user.test,
-      score: (score / test.answers.length) * 100,
-    });
+    return result;
   }
-  return result;
-}
+  
 
 const scoreTestButton = document.querySelector(".scoreTest");
 scoreTestButton.addEventListener("click", async () => {
   try {
-    const data = await getData();
-    const result = gradeTest(data);
+    const data = await getTestData();
+    const result = gradedTests(data);
     console.log(result);
     const resultsDiv = document.querySelector(".results");
     resultsDiv.innerHTML = "";
